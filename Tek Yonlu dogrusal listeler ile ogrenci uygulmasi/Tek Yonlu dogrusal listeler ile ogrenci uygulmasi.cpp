22668c4329d1d112f7f9b28e530f3b14d531d484
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2022-04-05 23:10:42 +0300

    update

---
+++ Tek Yonlu dogrusal listeler ile ogrenci uygulmasi/Tek Yonlu dogrusal listeler ile ogrenci uygulmasi.cpp
@@ -0,0 +1,203 @@
+﻿#include<iostream>
+
+using namespace std;
+
+struct node
+{
+	int no;
+	string ad;
+	string soyad;
+	int ort;
+	bool durum;
+	struct node* next;
+};
+
+struct node* ekle(struct node* head, int no, string ad, string soyad, int ort)
+{
+	struct node* ogrenci = new node();
+	ogrenci->no = no;
+	ogrenci->ad = ad;
+	ogrenci->soyad = soyad;
+	ogrenci->ort = ort;
+	ogrenci->durum = ort >= 50 ? true : false;
+
+	if (head == NULL)
+	{
+
+		head = ogrenci;
+		cout << head->no << "numarali ogrenci listedeki ilk kayda eklendi" << endl;
+		head->next = NULL;
+	}
+	else
+	{
+		ogrenci->next = head;
+		head = ogrenci;
+		cout << head->no << " numarali ogrenci listedeye eklendi" << endl;
+	}
+	return head;
+}
+
+struct node* bastanSİl(struct node* head)
+{
+	if (head == NULL)
+	{
+		cout << "Liste bos, herhangi  bir ogrenci kaydi yoktur" << endl;
+	}
+	else
+	{
+		if (head->next == NULL)
+		{
+			cout << head->no << "numarali ogrenci listeden silindi" << endl;
+			delete head;
+			head = NULL;
+		}
+		else
+		{
+			cout << head->no << "numarali ogrenci listeden silindi" << endl;
+			struct node* temp = head->next;
+			delete head;
+			head = temp;
+		}
+	}
+	return head;
+}
+
+struct node* sondanSİl(struct node* head)
+{
+	if (head == NULL)
+	{
+		cout << "Liste bos, herhangi  bir ogrenci kaydi yoktur" << endl;
+	}
+	else
+	{
+		if (head->next == NULL)
+		{
+			cout << head->no << "numarali ogrenci listeden silindi" << endl;
+			delete head;
+			head = NULL;
+		}
+		else
+		{
+			struct node* temp = head;
+			while (temp->next->next != NULL)
+			{
+				temp = temp->next;
+			}
+			cout << temp->next->no << "numarali ogrenci listeden silindi" << endl;
+
+			delete temp->next;
+			temp->next = NULL;
+		}
+	}
+	return head;
+}
+
+struct node* ara(struct node* head, int key)
+{
+	system("cls");
+	bool sonuc = false;
+	if (head == NULL)
+	{
+		cout << "Lİste bostur" << endl;
+	}
+	else
+	{
+		struct node* temp = head;
+		while (temp != NULL)
+		{
+			if (temp->no == key)
+			{
+				cout << key << "Numarali ogrenci bilgileri:" << endl;
+				cout << "Adi   :" << temp->ad << endl;
+				cout << "Soyad   :" << temp->soyad << endl;
+				cout << "Ortalama   :" << temp->ort << endl;
+				cout << "Durum   :" << temp->durum << endl;
+				sonuc = true;
+				break;
+			}
+			temp = temp->next;
+		}
+		if (!sonuc)
+		{
+			cout << key << " numarali kayit listede yoktur" << endl;
+		}
+	}
+	return head;
+}
+
+struct node* bilgileriGetir(struct node* head)
+{
+	system("cls");
+	if (head == NULL)
+	{
+		cout << "Liste bostur" << endl;
+	}
+	else
+	{
+		struct node* temp = head;
+		while (temp != NULL)
+		{
+			cout << temp->no << " Numarali ogrencinin adi:" << temp->ad << endl;
+			cout << "Soyad:" << temp->soyad << endl;
+			cout << "Ortalama: " << temp->ort << endl;
+			cout << "Durum:" << temp->durum << endl;
+			temp = temp->next;
+		}
+
+	}
+	return head;
+}
+int main()
+{
+	int no, ort, sayi, secim;
+	string ad, soyad;
+	bool durum;
+
+	struct node* head = NULL;
+	while (1)
+	{
+		cout << endl;
+		cout << "Tek Yonlu Dogrusal Bagli Liste Ogrenci Kayit Uygulamasi " << endl;
+		cout << "1--Yeni kayit" << endl;
+		cout << "2--Bastan sil" << endl;
+		cout << "3--Sondan sil" << endl;
+		cout << "4--Kayit ara" << endl;
+		cout << "5--Tum bilgileri getir" << endl;
+		cout << "0--CIKIS" << endl;
+		cin >> secim;
+		switch (secim)
+		{
+
+		case 1:
+			cout << "Yeni kaydi yapilacak ogrencinin bilgilerini giriniz" << endl;
+			cout << "Numara    :   "; cin >> no;
+			cout << "Ad        :   "; cin >> ad;
+			cout << "Soyad     :   "; cin >> soyad;
+			cout << "Ortalama  :   "; cin >> ort;
+			head = ekle(head, no, ad, soyad, ort);
+			break;
+		case 2:
+			head = bastanSİl(head);
+			break;
+		case 3:
+			head = sondanSİl(head);
+			break;
+		case 4:
+			cout << "Ogrenci numarasi:"; cin >> no;
+			head = ara(head, no);
+			break;
+		case 5:
+			head = bilgileriGetir(head);
+			break;
+		case 0:
+			cout << "Programdan Cikiliyor.....";
+			return 0;
+			break;
+
+		default:
+			cout << "Hatali secim yaptiniz.." << endl;
+			break;
+		}
+	}
+	return 0;
+}
---
+++ Tek yonlu dogrusal liste calismalar/Tek yonlu dogrusal liste calismalar.cpp
@@ -0,0 +1,278 @@
+﻿#include<iostream>
+
+using namespace std;
+
+struct node {                                          //yapımızı oluşturduk 
+	int data;
+	struct node* list;
+};
+
+struct node* basaEkle(struct node* head, int key)
+{
+	if (head == NULL)                                 //head düğüm listenin ilk düğümü
+	{
+		struct node* temp = new node();
+		temp->data = key;
+		temp->list = NULL;
+
+		head = temp;
+		cout << "ilk dugum olusturuldu" << endl;
+	}
+	else
+	{
+		struct node* temp = new node();
+		temp->data = key;
+		temp->list = head;
+
+		head = temp;                             //basa elelman ekledik ve head güncelledi	k
+		cout << "listenin basina dugumu ekleme islemi yapildi" << endl;
+	}
+	return head;
+};
+struct node* sonaEkle(struct node* head, int key)
+{
+	if (head == NULL)                                 //head düğüm listenin ilk düğümü
+	{
+		struct node* temp = new node();
+		temp->data = key;
+		temp->list = NULL;
+
+		head = temp;
+		cout << "ilk dugum olusturuldu" << endl;
+	}
+	else
+	{
+		struct node* temp = new node();
+		temp->data = key;
+		temp->list = NULL;
+
+		struct node* temp2 = head;
+		while (temp2->list != NULL)
+			temp2 = temp2->list;
+
+		temp2->list = temp;
+
+		cout << "sona ekleme islemi yapildi" << endl;
+	}
+	return head;
+};
+
+void verileriYazdir(struct node* head)
+{
+	system("cls");
+	if (head == NULL)
+		cout << "Liste bos" << endl;
+	else {
+		struct node* temp = head;
+		while (temp != NULL)
+		{
+			cout << temp->data << " ";
+			temp = temp->list;
+		}
+	}
+}
+void TumBilgileriYazdir(struct node* head)
+{
+	system("cls");
+	if (head == NULL)
+		cout << "Liste bos" << endl;
+	else {
+		struct node* temp = head;
+		int i = 1;
+		while (temp != NULL)
+		{
+			cout << i << " .dugumun data:" << temp->data << " adresi: " << temp << "bir sonraki dugumun adresi:" << temp->list << endl;;
+			temp = temp->list;
+			i++;
+		}
+	}
+}
+
+int dugumAdeti(struct node* head)
+{
+	int adet = 0;
+
+	if (head == NULL)
+	{
+		cout << " Liste bos!!" << endl;
+		return 0;
+	}
+	else
+	{
+		struct node* temp = head;
+		while (temp != NULL)
+		{
+			adet++;
+			temp = temp->list;
+		}
+		return adet;
+	}
+}
+int verilerinTopla(struct node* head)
+{
+	int toplam = 0;
+
+	if (head == NULL)
+	{
+		cout << " Liste bos!!" << endl;
+		return 0;
+	}
+	else
+	{
+		struct node* temp = head;
+		while (temp != NULL)
+		{
+			toplam += temp->data;                            //datalari topla
+			temp = temp->list;								//bir sonraki düğüme geçiyoruz
+		}
+		return toplam;
+	}
+}
+
+void veriAra(struct node* head, int arananSayi)
+{
+	bool sonuc = false;
+	if (head == NULL)
+		cout << "Liste bos" << endl;
+	else
+	{
+
+		int index = 0;
+
+		struct node* temp = head;
+		while (temp != NULL)
+		{
+			index++;
+			if (temp->data == arananSayi)
+			{
+				sonuc = true;
+				break;
+			}
+			temp = temp->list;
+		}
+		if (sonuc)
+			cout << arananSayi << " sayisi listede vardir ve dugum sirasi:" << index << endl;
+		else
+			cout << "Aranan sayi listede yoktur" << endl;
+	}
+
+}
+
+struct node* bastanSil(struct node* head)
+{
+	if (head == NULL)
+	{
+		cout << "Liste bostur silinecek dugum yoktur" << endl;
+	}
+	else
+	{
+		if (head->list == NULL)
+		{
+			delete head;
+			head = NULL;
+		}
+		else
+		{
+			struct node* temp = head->list;
+			delete head;                         //ilk düğümü sildim
+			head = temp;
+		}
+	}
+	return head;
+}
+struct node* sondanSil(struct node* head)
+{
+	if (head == NULL)
+	{
+		cout << "Liste bostur silinecek dugum yoktur" << endl;
+	}
+	else
+	{
+		if (head->list == NULL)
+		{
+			delete head;
+			head = NULL;
+		}
+		else
+		{
+			struct node* temp = head;
+			while (temp->list->list != NULL)
+				temp = temp->list;
+
+			struct node* temp2 = temp;
+			delete temp->list;
+			temp2->list = NULL;
+
+		}
+	}
+	return head;
+}
+int main()
+{
+	struct node* head = NULL;
+	int sayi, secim;
+
+	while (1)
+	{
+		cout << endl;
+		cout << "tek yonlu dogrusal bagli liste uygulamasi" << endl;
+		cout << "1--Basa eleman ekle" << endl;
+		cout << "2--Sona eleman ekle" << endl;
+		cout << "3--Verileri yazdir" << endl;
+		cout << "4--Listedeki tum bilgileri yazdir" << endl;
+		cout << "5--Dugum adeti" << endl;
+		cout << "6--Verilerin toplami" << endl;
+		cout << "7--Arama yap" << endl;
+		cout << "8--Bastan eleman sil" << endl;
+		cout << "9--Sondan sil" << endl;
+		cout << "10--CIKIS" << endl;
+		cin >> secim;
+		switch (secim)
+		{
+		case 1:
+			cout << "Basa eklenecek sayi:";
+			cin >> sayi;
+			head = basaEkle(head, sayi);
+			break;
+
+		case 2:
+			cout << "Sona eklenecek sayi:";
+			cin >> sayi;
+			head = sonaEkle(head, sayi);
+			break;
+		case 3:
+			verileriYazdir(head);
+			break;
+		case 4:
+			TumBilgileriYazdir(head);
+			break;
+		case 5:
+			cout << "Listedeki dugum adeti:" << dugumAdeti(head) << endl;
+			break;
+		case 6:
+			cout << "Listedeki verilerin toplami:" << verilerinTopla(head) << endl;
+		case 7:
+			cout << "Aranan sayiyi giriniz:";
+			cin >> sayi;
+			veriAra(head, sayi);
+			break;
+		case 8:
+			head = bastanSil(head);
+			break;
+		case 9:
+			head = sondanSil(head);
+			break;
+		case 10:
+			cout << "Cikis yapildi...";
+			return 0;
+
+		default:
+			cout << "Hatali secim yaptiniz" << endl;
+			break;
+		}
+	}
+
+
+
+	return 0;
+}
